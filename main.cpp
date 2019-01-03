3b8df934f1491cb06c4c3d24021b31a73e37b9e3
Agus Safarudin <42315161+agussafarudin@users.noreply.github.com>
2019-01-04 00:39:15 +0700

    v-01

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float m,c;
+    cout << "Softeare pembuat E=mc2 \n \n";
+    cout << "Masukkan nilai m =";
+    cin >>m;
+    cout<<"\n \n";
+    cout << "Masukkan nilai c =";
+    cin >>c;
+    cout<<"\n \n";
+    cout << "Hasilnya adalah  =";
+    cout <<(m*(c*c));
+}
